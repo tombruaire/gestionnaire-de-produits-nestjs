ed09f47b3d48e8fd8904c347bda980dbf67c69d5
Tom BRUAIRE <tombruaire@gmail.com>
2024-03-11 16:48:09 +0100

    Mise en place d'un logger de requête

--- src/middleware/logger.middleware.ts
+++ src/middleware/logger.middleware.ts
@@ -1,12 +1,20 @@
-import { Injectable, NestMiddleware } from "@nestjs/common";
-import { NextFunction } from "express";
+import { Injectable, NestMiddleware, Logger } from "@nestjs/common";
+import { NextFunction, Request, Response } from "express";
 
 @Injectable()
 export class RequestLoggerMiddleware implements NestMiddleware {
+    private logger = new Logger('RequestLoggerMiddleware');
+
     use(req: Request, res: Response, next: NextFunction): void {
-        console.log(
-            `Requête de type ${req.method} réceptionnée à ${new Date()}`,
-        );
+        const userAgent = req.headers['user-agent'] || 'Unknown';
+        const clientIp = req.ip || 'Unknown';
+        const requestUrl = req.originalUrl || req.url || 'Unknown';
+
+        this.logger.log(`Requête de type ${req.method} réceptionnée à ${new Date()}`);
+        this.logger.log(`Provenance: ${requestUrl}`);
+        this.logger.log(`Agent utilisateur: ${userAgent}`);
+        this.logger.log(`Adresse IP du client: ${clientIp}`);
+
         next();
     }
 }
